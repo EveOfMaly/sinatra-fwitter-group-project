4addda076cd67cf6a5d1dc32d51a55e9ebea534a
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-02-02 21:23:36 -0500

    tweets action setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug
+    self.username.to_s.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find {|user| user.slug == slug}
+  end
+  
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
 end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create(username: "Kim", email: "kimkim@gmail.com", password: "kiki")
+User.create(username: "Johm", email: "john@test.com", password: "johnpass")
+User.create(username: "James", email: "james@gmail.com", password: "jamespass")
